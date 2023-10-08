e8551520399d0182c5aef0d67c38d5b9d9209f76
Viktor Muromtcev <muromtsevvictor07@gmail.com>
2023-10-08 14:12:46 +0200

    practise update

--- for_practise/Program.cs
+++ for_practise/Program.cs
@@ -1,69 +1,27 @@
-﻿using System;
+﻿int[] schedule = {800, 1200, 1600, 2000};
 
-int[] times = {800, 1200, 1600, 2000};
-int diff = 0;
+DisplayAdjustedTime(schedule, 6, -6);
 
-Console.WriteLine("Enter current GMT");
-int currentGMT = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Current Medicine Schedule:");
-DisplayTimes();
-
-Console.WriteLine("Enter new GMT");
-int newGMT = Convert.ToInt32(Console.ReadLine());
-
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+void DisplayAdjustedTime(int[] times, int currentGMT, int newGMT)
 {
-    Console.WriteLine("Invalid GMT");
-}
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
-{
-    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    AdjustTime();
-} 
-else 
-{
-    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-    AdjustTime();
-}
-
-Console.WriteLine("New Medicine Schedule:");
-DisplayTimes();
-
-
-
-
-void DisplayTimes()
-{
-        /* Format and display medicine times */
-    foreach (int val in times)
+    int diff = 0;
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
     {
-        string time = val.ToString();
-        int len = time.Length;
-
-        if (len >= 3)
-        {
-            time = time.Insert(len - 2, ":");
-        }
-        else if (len == 2)
-        {
-            time = time.Insert(0, "0:");
-        }
-        else
-        {
-            time = time.Insert(0, "0:0");
-        }
-
-        Console.Write($"{time} ");
+        Console.WriteLine("Invalid GMT");
     }
-    Console.WriteLine();
-}
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    }
+    else 
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+    }
 
-void AdjustTime()
-{
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
     for (int i = 0; i < times.Length; i++) 
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
+{
+    int newTime = ((times[i] + diff)) % 2400;
+    Console.WriteLine($"{times[i]} -> {newTime}");
+}
+
 }
