6e74bfd6b5fb8e2f1a61279b828387992a0029fc
Krzysztof Balcerowski <Krzysztof_Balcerowski@epam.com>
2023-11-06 10:03:25 +0100

    Exercise1 to complete

--- basics/Exercise1.cs
+++ basics/Exercise1.cs
@@ -1,6 +1,19 @@
 ﻿namespace basics;
 
-public class Class1
-{
 
+/// <summary>
+/// Class should provide data object that includes information how many times given element occurs in the array
+/// example parameter object : List<int> numbers = new List<int>(){9,9,9,2,1,1,3,3,4,4}
+/// </summary>
+public class Exercise1
+{
+    public Dictionary<int,int> GetStatistics(List<int> numbers)
+    {
+        Dictionary<int, int> toReturn = new Dictionary<int, int>();
+
+        //logic
+
+
+        return toReturn;
+    }
 }
