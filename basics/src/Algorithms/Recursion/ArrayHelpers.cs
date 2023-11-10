28fd86480b550cfa963cc092c725c4af1858b860
Viktor Muromtcev <muromtsevvictor07@gmail.com>
2023-11-10 11:11:37 +0100

    Recursion theory added

---
+++ basics/src/Algorithms/Recursion/ArrayHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.Algorithms.Recursion
+{
+    public static class ArrayHelpers
+    {
+        public static void RecursiveReverse(this List<int> inputList, int currentIndex, int nextIndex)
+        {
+            if (inputList.Count == 1 || (currentIndex + 1) == nextIndex)
+            {
+                return;
+            }
+            else
+            {
+                int currentIndexValue = inputList[currentIndex];
+                int nextIndexValue = inputList[nextIndex];
+                inputList[nextIndex] = currentIndexValue;
+                inputList[currentIndex] = nextIndexValue;
+                inputList.RecursiveReverse(currentIndex + 1, nextIndex - 1);
+
+            }
+        }
+    }
+}
