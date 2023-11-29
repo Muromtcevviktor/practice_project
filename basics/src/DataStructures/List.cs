c9d31180564e69be99768ff68d10d1cd380568f0
Viktor Muromtcev <muromtsevvictor07@gmail.com>
2023-11-29 09:16:50 +0100

    GetItem method add

--- basics/src/DataStructures/List.cs
+++ basics/src/DataStructures/List.cs
@@ -1,4 +1,6 @@
 ﻿using Basics.Exceptions;
+using System;
+using System.Data;
 using System.Dynamic;
 
 namespace Basics.DataStructures
@@ -66,12 +68,12 @@ namespace Basics.DataStructures
     {
         public T Value { get; set; }
         public Node<T> Next { get; set; }
-
     }
 
-    public class List<T>
+    public class LinkedList<T>
     {
-        Node<T> head = null;
+        Node<T>? head;
+        Node<T>? tail;
 
         public void Add(T item)
         {
@@ -102,10 +104,60 @@ namespace Basics.DataStructures
             }
             else if (index > 0 && head != null) //TODO: homework  
             {
+                Node<T> currentItem = head;
+                int currentIndex = 0;
 
+                while (currentIndex < index && currentItem != null)
+                {
+                    currentItem = currentItem.Next;
+                    currentIndex++;
+                }
+
+                if (currentIndex == index)
+                {
+                    return currentItem!;
+                }
+                else
+                {
+                  throw new IndexOutOfRangeException("Node for given index does not exist.");
+                }  
             }
-            //TODO: throw exeption if Node for given index doesn not exist
+            else
+            {
+                throw new IndexOutOfRangeException("Node for given index does not exist.");
+            }
+            //TODO: throw exeption if Node for given index doesn not exist  
         }
 
     }
 }
+
+
+
+/*
+        public Node<T> GetItem(int index)
+        {
+            if (index < 0 || head == null)
+            {
+                throw new IndexOutOfRangeException("Node for given index does not exist.");
+            }
+
+            Node<T> current = head;
+            int currentIndex = 0;
+
+            while (currentIndex < index && current.Next != null)
+            {
+                current = current.Next;
+                currentIndex++;
+            }
+
+            if (currentIndex == index)
+            {
+                return current;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Node for given index does not exist.");
+            }
+        }
+*/
