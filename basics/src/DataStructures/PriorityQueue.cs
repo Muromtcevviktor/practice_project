34c5503736d3a2c2f0fa7f799e86a27d654b0c70
Viktor Muromtcev <muromtsevvictor07@gmail.com>
2023-12-08 17:26:09 +0100

    prioQueue + tests implement

---
+++ basics/src/DataStructures/PriorityQueue.cs
@@ -0,0 +1,35 @@
+﻿using Basics.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Security;
+using System.Security.Principal;
+
+namespace Basics.DataStructures
+{
+    public class PriorityQueue
+    {
+        private Heap heap = new Heap();
+
+        public int Count => heap.Count;
+
+        public int? Peek()
+        {
+            return heap.Peek();
+        }
+
+        public void Enqueue(int item)
+        {
+            heap.Add(item);
+        }
+
+        public int Dequeue()
+        {
+            return heap.Pop();
+        }
+    }
+
+}
